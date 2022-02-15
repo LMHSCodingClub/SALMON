9b8be74a8d5ab153fb09bb6b49009b05852b1ca0
David-Volchonok <57112433+David-Volchonok@users.noreply.github.com>
2022-02-15 14:40:42 -0500

    Create salmon.java
    Created file, wrote what to do

---
+++ salmon.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import static java.lang.System.*;
+import java.util.*;
+import java.util.Scanner;
+
+class Salmon
+{
+   public static void main (String str[]) throws IOException 
+   {
+     //Initialize the rooms, numbers, and distances
+     
+     //Read array for student directions
+     
+     //calculate distances
+     
+     //compare
+     
+     //output
+   }
+  
+}
+
+
