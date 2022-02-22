2290bdd2846f93c93804b06a2fa5df9efeda6e04
David-Volchonok <57112433+David-Volchonok@users.noreply.github.com>
2022-02-22 14:41:30 -0500

    Create Stairway.java
    Made it a thing

---
+++ Stairway.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import static java.lang.System.*;
+import java.util.*;
+//import java.util.Scanner;
+
+public class Stairway
+{
+  public double distance = 0;
+  public String name = "Stairway ";
+  public boolean up = false;
+  
+  Stairway(String in_name)
+  {
+    name += in_name;
+  }
+  
+  public double getDistance()
+  {
+   return distance; 
+  }
+  
+  public void addDistance(double add)
+  {
+    distance += add;
+  }
+  
+  public void setDirection(boolean input)
+  {
+   up = input; 
+  }
+  
+  public void setDirection(String input)
+  {
+   if(input.equals("up"))
+     up = true;
+   else
+     up = false;
+  }
+  
+}
