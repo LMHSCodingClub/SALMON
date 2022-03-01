8933d3c4bbcaedae2c6bf7e7e5ab2de999fa9394
David-Volchonok <57112433+David-Volchonok@users.noreply.github.com>
2022-03-01 15:14:48 -0500

    Update Room.java
    Floor 2 calculations added

--- Room.java
+++ Room.java
@@ -5,6 +5,10 @@ import java.util.*;
 
 public class Room
 {
+   final static double TOP_LENGTH = 40.7;
+   final static double TOP_WIDTH = 37.7;
+   final static double TOP_SPEC_CASE = 6; // for rooms 214 and 221
+   
    public int number = 0;
    public int floor = 0;
    
@@ -27,7 +31,48 @@ public class Room
       else
       {
         floor = 2;
-        //calculate distances
+        if(number < 206)
+        {
+           distance1 = designate;
+           distance2 = TOP_WIDTH - designate;
+           distance3 = distance2 + TOP_LENGTH;
+           distance4 = distance1 + TOP_LENGTH;
+        }
+         else if (number < 214)
+         {
+           distance1 = designate;
+           distance2 = distance1 + TOP_WIDTH;
+           distance4 = TOP_LENGTH - designate;
+           distance3 = distance4 + TOP_WIDTH;
+         }
+         else if (number == 214)
+         {
+           distance1 = TOPLENGTH + TOP_SPEC_CASE;
+           distance2 = distance1 + TOP_WIDTH;
+           distance4 = -TOP_SPEC_CASE;
+           distance3 = TOP_SPEC_CASE + TOP_WIDTH;
+         }
+         else if(number < 221)
+         {
+          distance1 = TOP_LENGTH + designate;
+          distance2 = TOP_LENGTH + TOP_WIDTH - desingate;
+          distance3 = TOP_WIDTH - designate;
+          distance4 = designate;  
+         }
+         else if(number == 221)
+         {
+          distance1 = TOP_LENGTH + TOP_WIDTH + TOP_SPEC_CASE;
+          distance2 = TOP_LENGTH + TOP_SPEC_CASE;
+          distance3 = -TOP_SPEC_CASE;
+          distance4 = TOP_WIDTH + TOP_SPEC_CASE;
+         }
+         else // all other rooms, 222-227
+         {
+          distance2 = designate;
+          distance1 = distance2 + TOP_WIDTH;
+          distance3 = TOP_LENGTH - designate;
+          distance4 = distance3 + TOP_WIDTH;
+         }
       }
 
    }
